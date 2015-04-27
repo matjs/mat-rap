ef98045264896952601ac9ad4ae2402f42806585
naij <naijser@gmail.com>
2015-04-27 10:19:22 +0800

    使用mat的代理功能获取资源

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
-var path     = require('path');
-var thunkify = require('thunkify');
-var fetch    = require('./lib/fetch');
+var rapAPIPrefix = 'rap.alibaba-inc.com/mockjsdata/'
 
 function rap(opts) {
-  opts = opts || {};
+  opts = opts || {}
 
   return function *rap(next) {
     if (/\.json$/.test(this.path)) {
-      opts.path = this.path;
-      opts.query = this.query;
-      opts.method = this.method;
-      
-      var data = yield thunkify(fetch)(opts);
-      
-      this.body = data;
-
-      return;
+      this.url = this.url.replace(this.host, rapAPIPrefix + opts.projectId)
     }
-    yield next;
-  };
+
+    yield next
+  }
 }
 
-module.exports = rap;
+module.exports = rap
