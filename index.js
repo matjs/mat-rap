437d488183a724646e82dc21ee95752b5121be9d
naij <naijser@gmail.com>
2015-05-28 20:47:09 +0800

    去掉接口后缀判断

--- index.js
+++ index.js
@@ -2,15 +2,8 @@ var path         = require('path')
 var rapAPIPrefix = 'rap.alibaba-inc.com/mockjsdata/'
 
 function rap(opts) {
-  opts = opts || {}
-  opts.ext = opts.ext || ['.json']
-
   return function *rap(next) {
-    var extname = path.extname(this.path)
-
-    if (~opts.ext.indexOf(extname)) {
-      this.url = this.url.replace(this.host, rapAPIPrefix + opts.projectId)
-    }
+    this.url = this.url.replace(this.host, rapAPIPrefix + opts.projectId)
 
     yield next
   }
