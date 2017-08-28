6a7e08dad48ed5d71eb377de47a0c155facfbc34
崇志 <chongzhi.ldq@taobao.com>
2017-08-28 16:12:41 +0800

    Feat: 支持rap2‘’

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-var path         = require('path')
-var rapAPIPrefix = 'rap.alibaba-inc.com/mockjsdata/'
+const rapAPIPrefixMap = {
+  1: 'rap.alibaba-inc.com/mockjsdata/',
+  2: 'rap2api.alibaba-inc.com/app/mock/'
+}
 
 function rap(opts) {
-  return function *rap(next) {
-    this.proxyPass = rapAPIPrefix + opts.projectId
+  return function* rap(next) {
+    let rapVersion = opts.rapVersion || '2' //默认用rap2
+    let rapAPIPrefix = rapAPIPrefixMap[rapVersion]
+
+    //rap2
+    if (rapVersion == '2') {
+      this.protocolAlias = 'https'
+      this.proxyPass = `${rapAPIPrefix + opts.projectId}/${this.request.method}`
+      this.request.method = 'GET'
+      this.isChangeOrigin = true //https需要设置header.host
+    } else {
+      //rap1
+      this.proxyPass = rapAPIPrefix + opts.projectId
+    }
+
     yield next
   }
 }
 
 module.exports = rap
+
+
+
+
+
+
+
+
+
