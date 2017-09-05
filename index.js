71bf2dadb2920a2e4e3453702f9204843bf55f69
崇志 <chongzhi.ldq@taobao.com>
2017-09-05 17:10:15 +0800

    Fix: 改成在mat-rap-verfiy插件里获取原始method值

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ function rap(opts) {
     if (rapVersion == '2') {
       this.protocolAlias = 'https'
       this.proxyPass = `${rapAPIPrefix + opts.projectId}/${this.request.method}`
-      this.request.methodOrigin = this.request.method
       this.request.method = 'GET'
       this.isChangeOrigin = true //https需要设置header.host
     } else {
